d1cd3033d5eda6cc3a13deec2980585bcef0009f
dvhafly87 <dvhafly87@gmail.com>
2025-07-15 12:26:21 +0900

    aa

--- src/main/java/com/MooneyB/llm3/Llm3Controller.java
+++ src/main/java/com/MooneyB/llm3/Llm3Controller.java
@@ -1,5 +1,6 @@
 package com.MooneyB.llm3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,16 @@ public class Llm3Controller {
 	public Llm3Controller(Llama3Service llama3Service) {
 		this.lm3 = llama3Service;
 	}
-
+	
 	@PostMapping("/llama3-api")
 	public ResponseEntity<?> GeneratingFirstMessage(
 			HttpServletRequest req,
-			@RequestParam("userInput") String userinput
+			@RequestParam("userInput") String userinput,
+			@RequestParam("userinfo") String useinfo
 			) {
-		System.out.println(userinput);
-		
-		String result = lm3.generateFromLlama3(userinput);
-		return ResponseEntity.ok(result);
+		System.out.println(userinput + useinfo);
+		lm3.DataAnalyzing(userinput, useinfo);
+//		String result = lm3.generateFromLlama3(userinput);
+		return ResponseEntity.ok(null);
 	}
 }
